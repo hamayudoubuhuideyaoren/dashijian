c6f941700708463f07eb5aeaa41a5e9f7f65d225
guagua <guagua@888.com>
2020-09-03 16:22:00 +0800

    bug

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 $(function () {
-    console.log(1);
+
 
     $("#link_reg").on("click", function () {
         $(".login-box").hide()
@@ -11,10 +11,45 @@ $(function () {
         $(".reg-box").hide()
     })
 
+
+
     // 自定义校验规则
     var form = layui.form
+    var layer = layui.layer
     form.verify({
         // 定义一个pwd校验规则
-        pwd: [/^[\S]{6,12}$/, '密码必须是6-12位，且不能出现空格']
+        pwd: [/^[\S]{6,12}$/, '密码必须是6-12位，且不能出现空格'],
+
+        // 校验两次密码是否一致
+        repwd: function (value) {
+            // 通过形参拿到的是确认密码框中的内容
+            // 还需要拿到密码框中的内容
+            // 然后进行一次等于的判断
+            // 如果判断失败,则return一个提示消息即可
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致！'
+            }
+        }
+    })
+
+
+    // 注册表单
+    $('#form_reg').on('submit', function (e) {
+        // 1. 阻止默认的提交行为
+        e.preventDefault()
+        // 2. 发起Ajax的POST请求
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登录！')
+            // 模拟人的点击行为
+            // $('#link_login').click()
+        })
     })
 })
